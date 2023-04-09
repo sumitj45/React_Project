bf28393ac5c89710ad2a4ac6f937135d7f32e679
Sumit <sumitj00112233@gmail.com>
2023-04-09 22:41:49 +0530

    delted and added files and components

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,13 +1,51 @@
-
-import './App.css';
-import Homepage from './pages/homepage';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import RecipeCard from "./components/RecipeCard";
+const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
-  return (
-    <div className="App">
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  
+  // search for the recipe
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = searchApi + query
+    const res = await fetch(url);
+    const data = await res.json();
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
 
-      <Homepage/>
-    
+  useEffect(() => {
+    searchRecipes()
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
+  }
+
+  return (
+    <div className="container">
+      <h2>Our Food Recipes</h2>
+      <SearchBar
+        isLoading={isLoading}
+        query={query}
+        setQuery={setQuery}
+        handleSubmit={handleSubmit}
+      />
+      <div className="recipes">
+        
+        {recipes ? recipes.map(recipe => (
+          <RecipeCard
+             key={recipe.idMeal}
+             recipe={recipe}
+          />
+        )) : "No Result Found."}
+      </div>
     </div>
   );
 }
